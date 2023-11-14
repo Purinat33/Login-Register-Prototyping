3346572db6899c5fa75fd3f0eff4ade4e4e50b66
Purinat33 <Purinat33@gmail.com>
2023-11-14 11:38:35 +0700

    Update database

--- index.js
+++ index.js
@@ -15,8 +15,60 @@ const staff_routes = require('./routes/employee')
 const {db} = require('./model/db')
 
 //Is elden.ring the username exists? if not add it
-staff_username = 'elden.ring';
-staff_password = 'rosebud';
+staffUsername = 'elden.ring';
+staffPassword = 'rosebud';
+
+// Check if the username already exists in the user_record table
+db.get('SELECT 1 FROM user_record WHERE user_username = ?', [staffUsername], (err, userRow) => {
+    if (err) {
+        console.error('Error checking if username exists in user_record:', err);
+        db.close();
+        return;
+    }
+
+    if (!userRow) {
+        // Username does not exist in user_record, proceed to check in employee_record
+        db.get('SELECT 1 FROM employee_record WHERE staff_username = ?', [staffUsername], (staffErr, staffRow) => {
+            if (staffErr) {
+                console.error('Error checking if username exists in employee_record:', staffErr);
+                db.close();
+                return;
+            }
+
+            if (!staffRow) {
+                // Username does not exist in employee_record, proceed to add a new staff member
+                bcrypt.hash(staffPassword, 10, (hashErr, hashedPassword) => {
+                    if (hashErr) {
+                        console.error('Error hashing password:', hashErr);
+                        db.close();
+                        return;
+                    }
+
+                    // Insert the new staff member into the employee_record table
+                    db.run('INSERT INTO employee_record (staff_username, staff_password) VALUES (?, ?)',
+                        [staffUsername, hashedPassword], (insertErr) => {
+                            if (insertErr) {
+                                console.error('Error inserting staff member:', insertErr);
+                            } else {
+                                console.log('Staff member added successfully.');
+                            }
+
+                            // Close the database connection after all operations are done
+                            db.close();
+                        });
+                });
+            } else {
+                // Username already exists in employee_record
+                console.log('Username already exists in employee_record.');
+                db.close();
+            }
+        });
+    } else {
+        // Username already exists in user_record
+        console.log('Username already exists in user_record.');
+        db.close();
+    }
+});
 
 //Some setting for our website
 app.use(morgan('dev'))
@@ -36,17 +88,6 @@ app.use('/', public_routes)
 app.use('/user', user_routes)
 app.use('/restricted', staff_routes)
 
-
-// We want to close the databases when we close the server (CTRL-C)
-process.on('SIGINT', () => {
-    db.close((err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Closed the database connection.');
-        process.exit(0);
-    });
-});
 
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`);
